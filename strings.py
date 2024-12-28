cb263109e9423a81f0fd61ec1bbd2ea1760af3dc
AvadhootKamble24 <avadhootkamble9@gmail.com>
2024-12-28 22:46:12 +0530

    updated strings.py

--- strings.py
+++ strings.py
@@ -2,33 +2,34 @@
 #print("GiraffeAcadamy")
 phrase = "avadhoot"
 
-# # print(phrase + " is cool") #strings concat
 
-# # print(phrase.upper()) #turns string into upper case
 
-# # print(phrase.lower()) #turns string into lower case
+print(phrase + " is cool") #strings concat
+print(phrase.upper()) #turns string into upper case
 
-# # print(phrase.isupper()) #return true if string is in upper case
+print(phrase.lower()) #turns string into lower case
 
-# # print(phrase.islower()) #return true if string is in lower case
+print(phrase.isupper()) #return true if string is in upper case
 
-# # print(phrase.lower().islower()) #turns to lower case and checks
+print(phrase.islower()) #return true if string is in lower case
 
-# # print(phrase.upper().isupper()) #turns to upper case and checks
+print(phrase.lower().islower()) #turns to lower case and checks
 
-# # print(len(phrase)) #returns length of string
+print(phrase.upper().isupper()) #turns to upper case and checks
 
-# # print(phrase[4]) #access individual using index
+print(len(phrase)) #returns length of string
 
-# # print(phrase.index("a")) #returns 'index' number
+print(phrase[4]) #access individual using index
 
-# print(phrase.replace("avadhoot","Kamble")) #replace 
+print(phrase.index("a")) #returns 'index' number
 
-# # print(phrase.index("a")) #returns 'index' number
+print(phrase.replace("avadhoot","Kamble")) #replace 
 
-# # print(phrase[4:7]) #slicing operator
+print(phrase.index("a")) #returns 'index' number
 
-# # x="""oranges
-# # are sweet""" #multi line string
+print(phrase[4:7]) #slicing operator
+
+x="""oranges
+are sweet""" #multi line string
 strings="hi my name is avadhoot"
 print("my" in strings)
