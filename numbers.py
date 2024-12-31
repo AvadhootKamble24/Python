0ee49cb11733d8bb552c1aa5688cef3a4ed89122
AvadhootKamble24 <avadhootkamble9@gmail.com>
2024-12-31 18:24:37 +0530

    added numbers.py

---
+++ numbers.py
@@ -0,0 +1,22 @@
+from math import *
+print(2)
+print(2.15)
+print(-2)
+print(-2.15456)
+print(2+5)
+print(2*(5+4))
+print(11%2)
+print("-------------")
+num = 5
+print(num)
+print(str(num)+ "is my fav number") #converts number to strings ,so numbers can be concatanated
+                                    # with strings
+num= -5
+print(abs(num)) #returns absolute value 
+print(pow(3,2)) #gives power to number
+print(max(4,67)) #returns largest num 
+print(min(54,-4)) #returns smallest num 
+print(round(3.45)) #rounds the number
+print(floor(4.3))
+print(ceil(3.6)) #works same as round
+print(sqrt(9)) #returns square root
