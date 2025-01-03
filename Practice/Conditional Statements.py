2a792b36cafc05fb833d56dba9cb764dc6e66ca7
AvadhootKamble24 <avadhootkamble9@gmail.com>
2025-01-03 18:45:51 +0530

    created practice folder and added 'Conditonal statement.py'

---
+++ Practice/Conditional Statements.py
@@ -0,0 +1,9 @@
+# Conditional Statements: Write a program that takes a number as input and prints whether the number is odd or even.
+
+num=int(input('Enter number'))
+
+if num%2==0:
+    print(num," is EVEN")
+    
+else:
+    print(num," is ODD")
